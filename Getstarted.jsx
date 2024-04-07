e11368f25c447bd10b54a7d0d4ff1c345f119cd3
Sripavanik <107269944+Sripavanik@users.noreply.github.com>
2024-04-07 20:03:18 +0530

    Add files via upload

---
+++ Getstarted.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Helnav from './GlassNavbarhel.jsx';
+import Dropdown from './Dropdown.jsx';
+import './getstarted.css';
+import flower from '../assets/robot.jpg';
+import profile from '../assets/profile.png';
+const Getstarted = () => {
+    return (
+        <div className='backg'>
+            <Helnav />
+            <p className='users'>Hey,User</p>
+          <a href=""><img src={profile} className="user_profiles"/></a>
+            <img src={flower} className="flower_vid" />
+            <div className="downpart">
+            <h1 className="getwelcome">Welcome, let's begin your journey to academic excellence!</h1>
+            <p className="selectoptions"> Please select the options below to start entering your marks.</p>
+            <div className="Dropdown">
+            <Dropdown/>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Getstarted;
